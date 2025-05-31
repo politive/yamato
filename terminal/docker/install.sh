8904b6c9368dc5d5f3e5c9ad01a9db5857743f6d
ysk1o <yusuke-fujii@politive.co.jp>
2025-05-31 15:13:12 +0900

    separate docker and docker-compose install script

--- terminal/docker/install.sh
+++ terminal/docker/install.sh
@@ -6,31 +6,3 @@ else
   run brew install docker
   log_installed "docker"
 fi
-
-log_section "Install Docker Compose"
-if command -v docker-compose &>/dev/null; then
-  log_skipped "docker-compose"
-else
-  run brew install docker-compose
-  log_installed "docker-compose"
-fi
-
-PLUGIN_DIR="$HOME/.docker/cli-plugins"
-PLUGIN_PATH="$PLUGIN_DIR/docker-compose"
-BREW_COMPOSE_BIN="$(brew --prefix docker-compose)/bin/docker-compose"
-
-run mkdir -p "$PLUGIN_DIR"
-
-if [ -L "$PLUGIN_PATH" ]; then
-  if [ "$(readlink "$PLUGIN_PATH")" = "$BREW_COMPOSE_BIN" ]; then
-    log_synlink_skipped "docker-compose plugin"
-  else
-    run ln -sf "$BREW_COMPOSE_BIN" "$PLUGIN_PATH"
-    log_synlink_replaced "docker-compose plugin"
-  fi
-elif [ -e "$PLUGIN_PATH" ]; then
-  log_skipped "docker-compose plugin"
-else
-  run ln -s "$BREW_COMPOSE_BIN" "$PLUGIN_PATH"
-  log_symlink "docker-compose plugin"
-fi
