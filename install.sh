e5af73ee0572b635d86aa2e38ba8426f800acd94
ysk1o <yusuke-fujii@politive.co.jp>
2025-10-22 22:12:54 +0900

    eval brew path after install homebrew

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if command -v brew >/dev/null 2>&1; then
   log_skipped "Homebrew"
 else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
   log_installed "Homebrew"
 fi
 
